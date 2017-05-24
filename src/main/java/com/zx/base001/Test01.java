f6f046c9ab22014ae89d35a09a3e80a551ecd7e9
BrightStarry <970389745@qq.com>
2017-05-24 23:36:25 +0800

    1

---
+++ src/main/java/com/zx/base001/Test01.java
@@ -0,0 +1,28 @@
+package com.zx.base001;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+
+		//-Xms5m -Xmx20m -XX:+PrintGCDetails -XX:+UseSerialGC -XX:+PrintCommandLineFlags
+		
+		//查看GC信息
+		System.out.println("max memory:" + Runtime.getRuntime().maxMemory());//最大内存
+		System.out.println("free memory:" + Runtime.getRuntime().freeMemory());//可用内存
+		System.out.println("total memory:" + Runtime.getRuntime().totalMemory());//已申请内存
+		
+		byte[] b1 = new byte[1*1024*1024];
+		System.out.println("分配了1M");
+		System.out.println("max memory:" + Runtime.getRuntime().maxMemory());
+		System.out.println("free memory:" + Runtime.getRuntime().freeMemory());
+		System.out.println("total memory:" + Runtime.getRuntime().totalMemory());
+		
+		byte[] b2 = new byte[4*1024*1024];
+		System.out.println("分配了4M");
+		System.out.println("max memory:" + Runtime.getRuntime().maxMemory());
+		System.out.println("free memory:" + Runtime.getRuntime().freeMemory());
+		System.out.println("total memory:" + Runtime.getRuntime().totalMemory());
+		
+	}
+	
+}
